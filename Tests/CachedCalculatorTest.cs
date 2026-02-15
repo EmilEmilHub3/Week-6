b5c67517f3e303a1091431fa965f61449025d449
EmilEmilHub2 <10464750@ucn.dk>
2026-02-15 19:16:59 +0100

    Nye tests

--- Tests/CachedCalculatorTest.cs
+++ Tests/CachedCalculatorTest.cs
@@ -5,7 +5,7 @@ namespace Tests;
 public class CachedCalculatorTest
 {
     [Test]
-    public void Add()
+    public void Add_FirstCall()
     {
         // Arrange
         var calc = new CachedCalculator();
@@ -17,5 +17,54 @@ public class CachedCalculatorTest
 
         // Assert
         Assert.That(result, Is.EqualTo(5));
+    }
+
+    [Test]
+    public void Add_SecondCall_UsesCache()
+    {
+        // Arrange
+        var calc = new CachedCalculator();
+        var a = 2;
+        var b = 3;
+
+        // Act
+        calc.Add(a, b); // first call (cache miss)
+        var result = calc.Add(a, b); // second call (cache hit)
+
+        // Assert
+        Assert.That(result, Is.EqualTo(5));
+        Assert.That(calc._cache.ContainsKey("2Add3"), Is.True);
+    }
+
+    [Test]
+    public void Factorial_CachesResult()
+    {
+        // Arrange
+        var calc = new CachedCalculator();
+        var n = 5;
+
+        // Act
+        calc.Factorial(n); // first call
+        var result = calc.Factorial(n); // second call (cached)
+
+        // Assert
+        Assert.That(result, Is.EqualTo(120));
+        Assert.That(calc._cache.ContainsKey("5Factorial"), Is.True);
+    }
+
+    [Test]
+    public void IsPrime_CachesResult()
+    {
+        // Arrange
+        var calc = new CachedCalculator();
+        var candidate = 13;
+
+        // Act
+        calc.IsPrime(candidate); // first call
+        var result = calc.IsPrime(candidate); // second call (cached)
+
+        // Assert
+        Assert.That(result, Is.True);
+        Assert.That(calc._cache.ContainsKey("13IsPrime"), Is.True);
     }
 }
--- Tests/SimpleCalculatorTest.cs
+++ Tests/SimpleCalculatorTest.cs
@@ -11,11 +11,155 @@ public class SimpleCalculatorTest
         var calc = new SimpleCalculator();
         var a = 2;
         var b = 3;
-        
+
         // Act
         var result = calc.Add(a, b);
-        
+
         // Assert
         Assert.That(result, Is.EqualTo(5));
+    }
+
+    [Test]
+    public void Subtract()
+    {
+        // Arrange
+        var calc = new SimpleCalculator();
+        var a = 5;
+        var b = 3;
+
+        // Act
+        var result = calc.Subtract(a, b);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(2));
+    }
+
+    [Test]
+    public void Multiply()
+    {
+        // Arrange
+        var calc = new SimpleCalculator();
+        var a = 4;
+        var b = 3;
+
+        // Act
+        var result = calc.Multiply(a, b);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(12));
+    }
+
+    [Test]
+    public void Divide()
+    {
+        // Arrange
+        var calc = new SimpleCalculator();
+        var a = 10;
+        var b = 2;
+
+        // Act
+        var result = calc.Divide(a, b);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(5));
+    }
+
+    [Test]
+    public void Divide_ByZero()
+    {
+        // Arrange
+        var calc = new SimpleCalculator();
+        var a = 10;
+        var b = 0;
+
+        // Act
+        TestDelegate action = () => calc.Divide(a, b);
+
+        // Assert
+        Assert.That(action, Throws.TypeOf<DivideByZeroException>());
+    }
+
+    [Test]
+    public void Factorial_Negative()
+    {
+        // Arrange
+        var calc = new SimpleCalculator();
+        var n = -1;
+
+        // Act
+        TestDelegate action = () => calc.Factorial(n);
+
+        // Assert
+        Assert.That(action, Throws.TypeOf<ArgumentException>());
+    }
+
+    [Test]
+    public void Factorial_Zero()
+    {
+        // Arrange
+        var calc = new SimpleCalculator();
+        var n = 0;
+
+        // Act
+        var result = calc.Factorial(n);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void Factorial_Positive()
+    {
+        // Arrange
+        var calc = new SimpleCalculator();
+        var n = 5;
+
+        // Act
+        var result = calc.Factorial(n);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(120));
+    }
+
+    [Test]
+    public void IsPrime_LessThanTwo()
+    {
+        // Arrange
+        var calc = new SimpleCalculator();
+        var candidate = 1;
+
+        // Act
+        var result = calc.IsPrime(candidate);
+
+        // Assert
+        Assert.That(result, Is.False);
+    }
+
+    [Test]
+    public void IsPrime_Composite()
+    {
+        // Arrange
+        var calc = new SimpleCalculator();
+        var candidate = 9;
+
+        // Act
+        var result = calc.IsPrime(candidate);
+
+        // Assert
+        Assert.That(result, Is.False);
+    }
+
+    [Test]
+    public void IsPrime_Prime()
+    {
+        // Arrange
+        var calc = new SimpleCalculator();
+        var candidate = 13;
+
+        // Act
+        var result = calc.IsPrime(candidate);
+
+        // Assert
+        Assert.That(result, Is.True);
     }
 }
