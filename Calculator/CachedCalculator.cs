c5507502300058013b535fc0613bd0d7d470cdf4
EmilEmilHub2 <10464750@ucn.dk>
2026-03-01 11:13:27 +0100

    calc

--- Calculator/CachedCalculator.cs
+++ Calculator/CachedCalculator.cs
@@ -6,7 +6,7 @@ public class CachedCalculator : ICalculator
 {
     private readonly SimpleCalculator _calculator = new();
     private readonly Dictionary<string, Calculation> _cache = new();
-    
+
     public int Add(int a, int b)
     {
         var calc = GetCachedResult<int>(a, b) ?? StoreInCache(_calculator.Add(a, b), a, b);
@@ -14,19 +14,19 @@ public class CachedCalculator : ICalculator
     }
 
     public int Subtract(int a, int b)
-    {        
+    {
         var calc = GetCachedResult<int>(a, b) ?? StoreInCache(_calculator.Subtract(a, b), a, b);
         return calc.Result;
     }
 
     public int Multiply(int a, int b)
-    {        
+    {
         var calc = GetCachedResult<int>(a, b) ?? StoreInCache(_calculator.Multiply(a, b), a, b);
         return calc.Result;
     }
 
     public int Divide(int a, int b)
-    {        
+    {
         var calc = GetCachedResult<int>(a, b) ?? StoreInCache(_calculator.Divide(a, b), a, b);
         return calc.Result;
     }
@@ -42,26 +42,23 @@ public class CachedCalculator : ICalculator
         var calc = GetCachedResult<bool>(candidate) ?? StoreInCache(_calculator.IsPrime(candidate), candidate);
         return calc.Result;
     }
-    
-    private Calculation<T>? GetCachedResult<T>(int a, int? b = null, [CallerMemberName]string operation = "")
+
+    private Calculation<T>? GetCachedResult<T>(int a, int? b = null, [CallerMemberName] string operation = "")
     {
-        var calc = new Calculation<T>(default, operation, a, b);
-        if (_cache.ContainsKey(calc.GetKey()))
-        {
-            return (Calculation<T>?)_cache[calc.GetKey()];
-        }
+        var key = Calculation.GetKey(a, operation, b);
+        if (_cache.TryGetValue(key, out var cached))
+            return cached as Calculation<T>;
         return null;
     }
-    
-    private Calculation<T> StoreInCache<T>(T result, int a, int? b = null, [CallerMemberName]string operation = "")
+
+    private Calculation<T> StoreInCache<T>(T result, int a, int? b = null, [CallerMemberName] string operation = "")
     {
         var calc = new Calculation<T>(result, operation, a, b);
-        calc.Result = result;
-        _cache.Add(calc.GetKey(), calc);
+        _cache[calc.GetKey()] = calc;
         return calc;
     }
-    
-    public class Calculation
+
+    private class Calculation
     {
         private int A { get; }
         private int? B { get; }
@@ -73,15 +70,15 @@ public class CachedCalculator : ICalculator
             B = b;
             Operation = operation;
         }
-    
-        public string GetKey()
-        {
-            return string.Concat(A, Operation, B);
-        }
+
+        public string GetKey() => GetKey(A, Operation, B);
+
+        public static string GetKey(int a, string operation, int? b) =>
+            string.Concat(a, operation, b);
     }
 
-    private class Calculation<T>(T? result, string operation, int a, int? b = null)
-    : Calculation(operation, a, b)
+    private sealed class Calculation<T>(T? result, string operation, int a, int? b = null)
+        : Calculation(operation, a, b)
     {
         public T? Result { get; set; } = result;
     }
