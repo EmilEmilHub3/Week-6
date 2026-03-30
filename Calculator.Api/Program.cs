779a40f3d55c30a5d3da610aebcf9163919cf7e8
EmilEmilHub2 <10464750@ucn.dk>
2026-03-30 13:40:28 +0200

    s

--- Calculator.Api/Program.cs
+++ Calculator.Api/Program.cs
@@ -1,6 +1,9 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+
+builder.WebHost.UseUrls("http://0.0.0.0:8080");
 
 var app = builder.Build();
 
